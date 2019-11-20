088b5f0af45b821ec73c021f8c698b1ffdb99559
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2019-11-20 13:45:19 -0500

    Update PM_PumpControl_NonStop_v3.ino

--- PM_PumpControl_NonStop_v3/PM_PumpControl_NonStop_v3.ino
+++ PM_PumpControl_NonStop_v3/PM_PumpControl_NonStop_v3.ino
@@ -5,7 +5,7 @@
   -tunid pid to p=3,i=2,d=.3 
 */
 
-
+//Hawes is working on this 
 #include <EEPROM.h>
 #include <PID_v1.h>
 #include <Wire.h>
@@ -258,4 +258,3 @@ void receiveEvent(int howMany) {  //set pump control, bp pressures, and motor sp
    inByte = "";
   }     
 }
-
